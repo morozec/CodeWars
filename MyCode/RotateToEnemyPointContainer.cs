dbec5aa03bf62b66a19a2b5fa861344c98a6c918
Andrey Morozov <andre15@inbox.ru>
2017-11-14 22:49:20 +0400

    Недостающий файл

---
+++ MyCode/RotateToEnemyPointContainer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.CodeGame.CodeWars2017.DevKit.CSharpCgdk.MyCode
+{
+    public class RotateToEnemyPointContainer
+    {
+        public Point RotateToEnemyPoint { get; set; }
+        public Point SameVehiclesPoint { get; set; }
+        public Point VehiclesPoint { get; set; }
+    }
+}
