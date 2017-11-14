e66d6e8c33cda88f2f33be101b8aa45036d22768
Andrey Morozov <andre15@inbox.ru>
2017-11-14 21:21:25 +0400

    Недостающие файлы

---
+++ MyCode/AStar.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Com.CodeGame.CodeRacing2015.DevKit.CSharpCgdk.AStar;
+using Com.CodeGame.CodeWars2017.DevKit.CSharpCgdk.Model;
+using IPA.AStar;
+
+namespace Com.CodeGame.CodeWars2017.DevKit.CSharpCgdk.MyCode
+{
+    public class AStar
+    {
+        public static double SquareSize = 74;
+        public static double BigWeight = 999999;
+
+        private const double _startX = 8;
+        private const double _startY = 8;
+
+        private ASquare[,] _table;
+        private IList<ASquare> _squares;
+        private ASquare _startSquare;
+        private int _n;
+        private int _m;
+
+        public AStar(World world)
+        {
+            _squares = new List<ASquare>();
+            _n = (int)(world.Width / SquareSize);
+            _m = (int)(world.Height / SquareSize);
+
+            _table = new ASquare[_n, _m];
+
+            for (var i = 0; i < _n; ++i)
+            {
+                for (var j = 0; j < _m; ++j)
+                {
+                    var square = new ASquare(
+                        SquareSize,
+                        _startX + i * SquareSize,
+                        _startY + j * SquareSize,
+                        1d,
+                        i + ":" + j);
+
+                    _squares.Add(square);
+
+                    _table[i, j] = square;
+                }
+            }
+
+            for (var i = 0; i < _n; ++i)
+            {
+                for (var j = 0; j < _m; ++j)
+                {
+                    var neighbors = new List<ASquare>();
+                    if (i != 0)
+                    {
+                        neighbors.Add(_table[i - 1, j]);
+                    }
+                    if (i != _n - 1)
+                    {
+                        neighbors.Add(_table[i + 1, j]);
+                    }
+                    if (j != 0)
+                    {
+                        neighbors.Add(_table[i, j - 1]);
+                    }
+                    if (j != _m - 1)
+                    {
+                        neighbors.Add(_table[i, j + 1]);
+                    }
+
+                    //if (i != 0 && j != 0)
+                    //{
+                    //    neighbors.Add(_table[i - 1, j - 1]);
+                    //}
+
+                    //if (i != _n - 1 && j != _m - 1)
+                    //{
+                    //    neighbors.Add(_table[i + 1, j + 1]);
+                    //}
+
+                    //if (i != 0 && j != _m - 1)
+                    //{
+                    //    neighbors.Add(_table[i - 1, j + 1]);
+                    //}
+
+                    //if (i != _n - 1 && j != 0)
+                    //{
+                    //    neighbors.Add(_table[i + 1, j - 1]);
+                    //}
+
+                    var square = _table[i, j];
+                    square.Neighbors = neighbors;
+                }
+            }
+        }
+
+        public void UpdateDynamicAStar(
+            int groupIndex, IList<Vehicle> vehicles, IDictionary<int, VehicleType> reversedGroupIndexes, MyStrategy ms)
+        {
+            var centerX = vehicles.Average(v => v.X);
+            var centerY = vehicles.Average(v => v.Y);
+
+            var myLeftX = centerX - SquareSize / 2;
+            var leftN = (int)(myLeftX / SquareSize);
+
+            var myTopY = centerY - SquareSize / 2;
+            var topM = (int)(myTopY / SquareSize);
+
+            for (var i = 0; i < _n; ++i)
+            {
+                for (var j = 0; j < _m; ++j)
+                {
+                    var square = _table[i, j];
+                    square.X = _startX + i * SquareSize;
+                    square.Y = _startY + j * SquareSize;
+                    square.Weight = 1d;
+                }
+            }
+
+            _startSquare = _table[leftN, topM];
+
+            foreach (var index in reversedGroupIndexes.Keys)
+            {
+                if (index == groupIndex) continue;
+                if (!MyStrategy.IsSameTypes(reversedGroupIndexes[groupIndex], reversedGroupIndexes[index]))
+                    continue; // они друг другу не мешают
+
+                var currVeichles = ms.GetVehicles(index, MyStrategy.Ownership.ALLY);
+                if (!currVeichles.Any()) continue; // все сдохли
+
+                var currCenterX = currVeichles.Average(v => v.X);
+                var currCenterY = currVeichles.Average(v => v.Y);
+
+                var currLeftX = currCenterX - SquareSize / 2;
+                var currLeftN = (int)(currLeftX / SquareSize);
+
+                var currTopY = currCenterY - SquareSize / 2;
+                var currTopM = (int)(currTopY / SquareSize);
+
+                _table[currLeftN, currTopM].Weight = BigWeight;
+            }
+
+
+            //if (leftN > 0 && _table[leftN - 1, topM].Weight == BigWeight)
+            //{
+            //    if (topM > 0) _table[leftN - 1, topM - 1].Weight = BigWeight;
+            //    if (topM < _m - 1) _table[leftN - 1, topM + 1].Weight = BigWeight;
+            //}
+
+            //if (topM > 0 && _table[leftN, topM - 1].Weight == BigWeight)
+            //{
+            //    if (leftN > 0) _table[leftN - 1, topM - 1].Weight = BigWeight;
+            //    if (leftN < _n - 1) _table[leftN + 1, topM - 1].Weight = BigWeight;
+            //}
+
+            //if (leftN < _n - 1 && _table[leftN + 1, topM].Weight == BigWeight)
+            //{
+            //    if (topM > 0) _table[leftN + 1, topM - 1].Weight = BigWeight;
+            //    if (topM < _m - 1) _table[leftN + 1, topM + 1].Weight = BigWeight;
+            //}
+
+            //if (topM < _m - 1 && _table[leftN, topM + 1].Weight == BigWeight)
+            //{
+            //    if (leftN > 0) _table[leftN - 1, topM + 1].Weight = BigWeight;
+            //    if (leftN < _n - 1) _table[leftN + 1, topM + 1].Weight = BigWeight;
+            //}
+        }
+
+        public int GetSquareI(double x)
+        {
+            var res = (int)((x - _startX) / SquareSize);
+            if (res < 0) return 0;
+            if (res > _n - 1) return _n - 1;
+            return res;
+        }
+
+        public int GetSquareJ(double y)
+        {
+            var res = (int)((y - _startY) / SquareSize);
+            if (res < 0) return 0;
+            if (res > _m - 1) return _m - 1;
+            return res;
+        }
+
+        public IList<APoint> GetPath(int startI, int startJ, int goalI, int goalJ)
+        {
+            var path = Calculator.GetPath(_table[startI, startJ], _table[goalI, goalJ], _squares);
+            return path;
+        }
+
+        public ASquare GetSupportAPoint(int goalI, int goalJ)
+        {
+            ASquare resSquare = null;
+            if (goalI > 0 && _table[goalI - 1, goalJ].Weight < BigWeight)
+            {
+                resSquare = _table[goalI - 1, goalJ];
+            }
+            else if (goalJ > 0 && _table[goalI, goalJ - 1].Weight < BigWeight)
+            {
+                resSquare = _table[goalI, goalJ - 1];
+            }
+            else if (goalI > 0 && goalJ > 0 && _table[goalI - 1, goalJ - 1].Weight < BigWeight)
+            {
+                resSquare = _table[goalI - 1, goalJ - 1];
+            }
+            return resSquare;
+        }
+
+        public IList<APoint> GetStraightPath(IList<APoint> path)
+        {
+            var result = new List<APoint>() {path[0]};
+         
+            var dx = (path[1] as ASquare).X - (path[0] as ASquare).X;
+            var dy = (path[1] as ASquare).Y - (path[0] as ASquare).Y;
+
+            for (var i = 2; i < path.Count; ++i)
+            {
+                var currDx = (path[i] as ASquare).X - (path[i - 1] as ASquare).X;
+                var currDy = (path[i] as ASquare).Y - (path[i - 1] as ASquare).Y;
+                if (dx != currDx || dy != currDy)
+                {
+                    result.Add(path[i-1]);
+                    dx = currDx;
+                    dy = currDy;
+                }
+            }
+
+            result.Add(path[path.Count - 1]);
+            return result;
+        }
+    }
+}
+
---
+++ MyCode/MathHelper.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Com.CodeGame.CodeWars2017.DevKit.CSharpCgdk.Model;
+
+namespace Com.CodeGame.CodeWars2017.DevKit.CSharpCgdk.MyCode
+{
+    public static class MathHelper
+    {
+        public static Rectangle GetGroupRectangle(IList<Vehicle> vehicles)
+        {
+            var x1 = vehicles.Min(v => v.X);
+            var x2 = vehicles.Max(v => v.X);
+            var y1 = vehicles.Min(v => v.Y);
+            var y2 = vehicles.Max(v => v.Y);
+
+            return new Rectangle
+            {
+                Points = new List<Point>
+                {
+                    new Point(x1, y1),
+                    new Point(x1, y2),
+                    new Point(x2, y2),
+                    new Point(x2, y1)
+                }
+            };
+        }
+
+
+        public static double GetAnlge(Vector v1, Vector v2)
+        {
+            var scalarMult = v1.V.X * v2.V.X + v1.V.Y * v2.V.Y;
+            var cos = scalarMult / v1.Length / v2.Length;
+            var angle = Math.Acos(cos);
+            var rotate = Rotate(v1.P1, v1.P2, v2.P2);
+            return rotate < 0 ? angle : -angle;
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns>+, если точка справа. -, если точка слева</returns>
+        private static double Rotate(Point a, Point b, Point c)
+        {
+            //-, ибо другое расположение осей x-y
+            return -((b.X - a.X) * (c.Y - b.Y) - (b.Y - a.Y) * (c.X - b.X));
+        }
+
+        private static bool Intersect(Point a, Point b, Point c, Point d)
+        {
+            return Rotate(a, b, c) * Rotate(a, b, d) <= 0 && Rotate(c, d, a) * Rotate(c, d, b) < 0;
+        }
+
+        private static bool PointLoc(Rectangle rect, Point a)
+        {
+            var n = rect.Points.Count;
+            if (Rotate(rect.Points[0], rect.Points[1], a) < 0 || Rotate(rect.Points[0], rect.Points[n - 1], a) > 0)
+                return false;
+            var p = 1;
+            var r = n - 1;
+            var q = 0;
+            while (r - p > 1)
+            {
+                q = (p + r) / 2;
+                if (Rotate(rect.Points[0], rect.Points[q], a) < 0)
+                {
+                    r = q;
+                }
+                else
+                {
+                    p = q;
+                }
+            }
+
+            return !Intersect(rect.Points[0], a, rect.Points[p], rect.Points[r]);
+        }
+
+
+        public static bool IsIntersects(Rectangle movingRectangle, Rectangle stayingRectangle, Point destPoint)
+        {
+            var points = stayingRectangle.Points.ToList();
+            points.Add(new Point(points.Average(p => p.X), points.Average(p => p.Y)));
+
+            var pathRectangle = new Rectangle { Points = new List<Point>() };
+            if (destPoint.X > 0 && destPoint.Y > 0)
+            {
+                pathRectangle.Points.Add(new Point(movingRectangle.Points[0].X, movingRectangle.Points[0].Y));
+                pathRectangle.Points.Add(new Point(movingRectangle.Points[1].X, movingRectangle.Points[1].Y));
+                pathRectangle.Points.Add(new Point(movingRectangle.Points[1].X + destPoint.X,
+                    movingRectangle.Points[1].Y + destPoint.Y));
+                pathRectangle.Points.Add(new Point(movingRectangle.Points[2].X + destPoint.X,
+                    movingRectangle.Points[2].Y + destPoint.Y));
+                pathRectangle.Points.Add(new Point(movingRectangle.Points[3].X + destPoint.X,
+                    movingRectangle.Points[3].Y + destPoint.Y));
+                pathRectangle.Points.Add(new Point(movingRectangle.Points[3].X, movingRectangle.Points[3].Y));
+            }
+            else if (destPoint.X < 0 && destPoint.Y > 0)
+            {
+                pathRectangle.Points.Add(new Point(movingRectangle.Points[0].X, movingRectangle.Points[0].Y));
+                pathRectangle.Points.Add(new Point(movingRectangle.Points[0].X + destPoint.X,
+                    movingRectangle.Points[0].Y + destPoint.Y));
+                pathRectangle.Points.Add(new Point(movingRectangle.Points[1].X + destPoint.X,
+                    movingRectangle.Points[1].Y + destPoint.Y));
+                pathRectangle.Points.Add(new Point(movingRectangle.Points[2].X + destPoint.X,
+                    movingRectangle.Points[2].Y + destPoint.Y));
+                pathRectangle.Points.Add(new Point(movingRectangle.Points[2].X, movingRectangle.Points[2].Y));
+                pathRectangle.Points.Add(new Point(movingRectangle.Points[3].X, movingRectangle.Points[3].Y));
+            }
+            else if (destPoint.X > 0 && destPoint.Y < 0)
+            {
+                pathRectangle.Points.Add(new Point(movingRectangle.Points[0].X, movingRectangle.Points[0].Y));
+                pathRectangle.Points.Add(new Point(movingRectangle.Points[1].X, movingRectangle.Points[1].Y));
+                pathRectangle.Points.Add(new Point(movingRectangle.Points[2].X, movingRectangle.Points[2].Y));
+                pathRectangle.Points.Add(new Point(movingRectangle.Points[2].X + destPoint.X,
+                    movingRectangle.Points[2].Y + destPoint.Y));
+                pathRectangle.Points.Add(new Point(movingRectangle.Points[3].X + destPoint.X,
+                    movingRectangle.Points[3].Y + destPoint.Y));
+                pathRectangle.Points.Add(new Point(movingRectangle.Points[1].X + destPoint.X,
+                    movingRectangle.Points[1].Y + destPoint.Y));
+            }
+            else if (destPoint.X < 0 && destPoint.Y < 0)
+            {
+                pathRectangle.Points.Add(new Point(movingRectangle.Points[1].X, movingRectangle.Points[1].Y));
+                pathRectangle.Points.Add(new Point(movingRectangle.Points[2].X, movingRectangle.Points[2].Y));
+                pathRectangle.Points.Add(new Point(movingRectangle.Points[3].X, movingRectangle.Points[3].Y));
+                pathRectangle.Points.Add(new Point(movingRectangle.Points[3].X + destPoint.X,
+                    movingRectangle.Points[3].Y + destPoint.Y));
+                pathRectangle.Points.Add(new Point(movingRectangle.Points[1].X + destPoint.X,
+                    movingRectangle.Points[1].Y + destPoint.Y));
+                pathRectangle.Points.Add(new Point(movingRectangle.Points[2].X + destPoint.X,
+                    movingRectangle.Points[2].Y + destPoint.Y));
+            }
+
+            foreach (var point in points)
+            {
+                if (PointLoc(pathRectangle, point)) return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        ///     Находит дальнюю точку пересчечения окружности и прямой, проходящей через центр окружности
+        /// </summary>
+        /// <param name="a">центр окружности</param>
+        /// <param name="b">вторая точка прямой</param>
+        /// <param name="c">точка на краю окружности</param>
+        /// <returns></returns>
+        public static Point GetLineCircleBehindCrossPoint(Point a, Point b, Point c)
+        {
+            var radius = a.GetDistance(c);
+            var x1 = a.X + radius / Math.Sqrt(1 + Math.Pow((b.Y - a.Y) / (b.X - a.X), 2));
+            var x2 = a.X - radius / Math.Sqrt(1 + Math.Pow((b.Y - a.Y) / (b.X - a.X), 2));
+
+            var y1 = (x1 - a.X) * (b.Y - a.Y) / (b.X - a.X) + a.Y;
+            var y2 = (x2 - a.X) * (b.Y - a.Y) / (b.X - a.X) + a.Y;
+
+            var p1 = new Point(x1, y1);
+            var p2 = new Point(x2, y2);
+
+            return b.GetDistance(p1) > b.GetDistance(p2) ? p1 : p2;
+        }
+
+    }
+}
